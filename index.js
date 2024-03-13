4bb1094578865955ec500cc34dabe8c4749d8311
CarlosDHH <charlydhh18@gmail.com>
2024-03-13 00:48:42 -0600

    Please enter the commit message for your changes. Lines starting  with '#' will be ignored, and an empty message aborts the commit.
     On branch main
     Your branch is up to date with 'origin/main'.
    
     Changes to be committed:
    	modified:   index.js

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const tipoUsuarioSchema=require('./src/routes/tipoUsuario')
 const usuarioSchema=require('./src/routes/usuarios')
 
 const app =express()
-const port= process.env.port||3000
+const port= process.env.PORT||3000
 
 //midlewares
 app.use(express.json())
