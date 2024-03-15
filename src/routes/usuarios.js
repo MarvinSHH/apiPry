e2fc47eb7599c908abe7f14ac93a67a1fd7ca35a
CarlosDHH <charlydhh18@gmail.com>
2024-03-14 23:58:26 -0600

    Please enter the commit message for your changes. Lines starting  with '#' will be ignored, and an empty message aborts the commit.
     On branch main
     Your branch is up to date with 'origin/main'.
    
     Changes to be committed:
    	modified:   package-lock.json
    	modified:   package.json
    	modified:   src/routes/usuarios.js

--- src/routes/usuarios.js
+++ src/routes/usuarios.js
@@ -1,7 +1,38 @@
 const express=require('express')
+const bcrypt =require('bcryptjs')
+const jwt = require('jsonwebtoken')
+
 const esquema=require('../models/usuarios')
 
 const router=express.Router()
+
+// Endpoint de inicio de sesión
+router.post('/login', async (req, res) => {
+    // La lógica de inicio de sesión va aquí
+    try {
+        const usuario = await Usuario.findOne({ correo: req.body.correo });
+        if (!usuario) {
+            return res.status(404).send('Usuario no encontrado');
+        }
+
+        const contraseñaValida = await bcrypt.compare(req.body.contraseña, usuario.contraseña);
+        if (!contraseñaValida) {
+            return res.status(401).send('Contraseña incorrecta');
+        }
+
+        // Generar token JWT
+        const token = jwt.sign(
+            { _id: usuario._id, tipo: usuario.tipo }, 
+            'tuSecretKey', // Reemplaza 'tuSecretKey' con tu clave secreta real
+            { expiresIn: '24h' }
+        );
+
+        res.json({ token }); // Envía el token al cliente
+    } catch (error) {
+        res.status(500).send('Error en el servidor');
+    }
+});
+
 
 router.get('/usuarios/x',(req,res)=>{
     res.json({"response":"Prueba Users"})
