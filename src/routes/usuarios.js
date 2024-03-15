1e7c455abeb7adf7238b7c9f7c60f3bcfe41f755
CarlosDHH <charlydhh18@gmail.com>
2024-03-15 01:56:15 -0600

    modified:   src/routes/usuarios.js

--- src/routes/usuarios.js
+++ src/routes/usuarios.js
@@ -14,10 +14,10 @@ router.post('/usuarios/login', async (req, res) => {
             return res.status(404).send('Usuario no encontrado');
         }
 
-        const contraseñaValida = await bcrypt.compare(req.body.contraseña, usuario.contraseña);
+        const contraseñaValida = await esquema.findOne({ contraseña: req.body.contraseña });
         const pass= contraseñaValida;
         if (!contraseñaValida) {
-            return res.status(401).send("Invalid Credentials");
+            return res.status(401).send("passwrong");
         }
 
         const token = jwt.sign(
